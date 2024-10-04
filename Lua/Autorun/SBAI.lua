4c8662ec13ad62b99a836e53d3058a69068c7da6
Dale Stager <dstager.lb@gmail.com>
2024-10-04 01:11:43 -0400

    Fixed bug when findDeconstructor() returned a nil value Added proper Lua workspace

--- Lua/Autorun/SBAI.lua
+++ Lua/Autorun/SBAI.lua
@@ -4,5 +4,5 @@ SBAI.Path = ...
 
 -- Deconstruct only within the sub
 Hook.Patch("PrintDeconstructor", "Barotrauma.AIObjectiveDeconstructItem", "FindDeconstructor", function(instance, ptable)
-    if not ptable.ReturnValue.Item.InPlayerSubmarine then return nil end
+    if ptable.ReturnValue and not ptable.ReturnValue.Item.InPlayerSubmarine then return nil end
 end, Hook.HookMethodType.After)
