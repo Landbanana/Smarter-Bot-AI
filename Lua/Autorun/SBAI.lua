a286a0b56bb74a8e3d8ef361500040fbe3037494
Dale Stager <dstager.lb@gmail.com>
2024-10-06 00:53:34 -0400

    Added check for whether sub has a deconstructor or not Maybe(?) optimized calls to getting the list of Deconstructors

--- Lua/Autorun/SBAI.lua
+++ Lua/Autorun/SBAI.lua
@@ -3,5 +3,6 @@ SBAI = {}
 SBAI.Path = ...
 
 if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
+    dofile(SBAI.Path.."/Lua/SBAI/Server/ItemHelper.Lua")
     dofile(SBAI.Path.."/Lua/SBAI/Server/AiObjective.Lua")
 end
--- Lua/SBAI/Server/AIObjective.lua
+++ Lua/SBAI/Server/AIObjective.lua
@@ -1,4 +1,29 @@
--- Deconstruct only within the sub
-Hook.Patch("PrintDeconstructor", "Barotrauma.AIObjectiveDeconstructItem", "FindDeconstructor", function(instance, ptable)
-    if ptable.ReturnValue and (not ptable.ReturnValue.Item.InPlayerSubmarine) then return nil end
-end, Hook.HookMethodType.After)
+LuaUserData.RegisterType("Barotrauma.AIObjectiveDeconstructItem") 
+
+-- deconstruct only within the sub if deconstructor is within sub, with optimized calls to get all deconstructors in map
+Hook.Patch("SBAI.FindDeconstructor", "Barotrauma.AIObjectiveDeconstructItem", "FindDeconstructor", function(instance, ptable)
+    ptable.PreventExecution = true
+
+    local closestDeconstructor = nil
+    local bestDistFactor = 0
+
+    for attempt = 1, 2, 1 do
+        for i, potentialDeconstructor in ipairs(SBAI.DeconstructorList) do
+            local distFactor
+            if potentialDeconstructor == nil then goto continue end
+            if potentialDeconstructor.InputContainer == nil then goto continue end
+            if not potentialDeconstructor.InputContainer.Inventory.CanBePut(instance.Item) then goto continue end
+            if not potentialDeconstructor.Item.HasAccess(instance.character) then goto continue end
+            distFactor = AIObjective.GetDistanceFactor(instance.Item.WorldPosition, potentialDeconstructor.Item.WorldPosition, 0.2)
+            if distFactor > bestDistFactor then
+                closestDeconstructor = potentialDeconstructor
+                bestDistFactor = distFactor
+            end
+            ::continue::
+        end
+
+        if closestDeconstructor == nil and attempt == 1 then SBAI.GenerateDeconstructorList() end
+    end
+
+    return closestDeconstructor
+end, Hook.HookMethodType.Before)
