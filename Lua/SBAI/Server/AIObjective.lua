ef2a0b906488c23398323fdfdb39ccfe2172c3e4
Dale Stager <dstager.lb@gmail.com>
2024-10-06 23:40:37 -0400

    Added (hopefully) handcuffed check for any sort of combat

--- Lua/SBAI/Server/AIObjective.lua
+++ Lua/SBAI/Server/AIObjective.lua
@@ -1,6 +1,6 @@
 LuaUserData.RegisterType("Barotrauma.AIObjectiveDeconstructItem") 
 
--- deconstruct only within the sub if deconstructor is within sub, with optimized calls to get all deconstructors in map
+-- Deconstruct Items: Deconstruct only within the sub if deconstructor is within sub, with optimized calls to get all deconstructors in map
 Hook.Patch("SBAI.FindDeconstructor", "Barotrauma.AIObjectiveDeconstructItem", "FindDeconstructor", function(instance, ptable)
     ptable.PreventExecution = true
 
@@ -28,7 +28,12 @@ Hook.Patch("SBAI.FindDeconstructor", "Barotrauma.AIObjectiveDeconstructItem", "F
     return closestDeconstructor
 end, Hook['HookMethodType'].Before)
 
--- Prevent attacking any handcuffed people, regardless of being knocked down
-Hook.Patch("SBAI.PreventAttackingHandcuffed", "Barotrauma.AIObjectiveFightIntruders", "IsValidTarget", function(instance, ptable)
+-- Fight Intruders: Prevent attacking any handcuffed people, regardless of being knocked down
+Hook.Patch("SBAI.PreventAttackingHandcuffedFightIntruders", "Barotrauma.AIObjectiveFightIntruders", "IsValidTarget", function(instance, ptable)
     return ptable.ReturnValue and not ptable["target"].IsHandcuffed
 end, Hook['HookMethodType'].After)
+
+-- Operate Weapons: Prevent attacking handcuffed people
+Hook.Patch("SBAI.PreventAttackingHandcuffedCombat", "Barotrauma.AIObjectiveCombat", "GetPriority", function(instance, ptable)
+    if instance.Enemy.IsHandcuffed then return 0 end
+end, Hook['HookMethodType'].Before)
