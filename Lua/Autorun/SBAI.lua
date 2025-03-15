e3880155765c63c7a35e4c6783d997e8a0515973
Dale Stager <dstager.lb@gmail.com>
2025-03-15 12:18:22 -0400

    Reorganized to make more sense

--- Lua/Autorun/SBAI.lua
+++ Lua/Autorun/SBAI.lua
@@ -1,8 +1,6 @@
-SBAI = {}
-
-SBAI.Path = ...
-
 if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
+    SBAI = {}
+    SBAI.Path = ...
     dofile(SBAI.Path.."/Lua/SBAI/Server/ItemHelper.Lua")
     dofile(SBAI.Path.."/Lua/SBAI/Server/AiObjective.Lua")
 end
