d3960697fd9b7212fa7ab624ca5a1fc54a819be7
Dale Stager <dstager.lb@gmail.com>
2025-03-15 12:18:15 -0400

    Cleaned up some unused code

--- Lua/SBAI/Server/AIObjective.lua
+++ Lua/SBAI/Server/AIObjective.lua
@@ -37,15 +37,3 @@ end, Hook['HookMethodType'].After)
 Hook.Patch("SBAI.PreventAttackingHandcuffedCombat", "Barotrauma.AIObjectiveCombat", "GetPriority", function(instance, ptable)
     if instance.Enemy.IsHandcuffed then return 0 end
 end, Hook['HookMethodType'].Before)
-
--- -- Cleanup Items: Don't pick up pet food that is within a certain distance of a pet
--- Hook.Patch("SBAI.DontStarvePets", "Barotrauma.AIObjectiveCleanupItems", "IsValidTarget",
---     {"Barotrauma.Item", "Barotrauma.Character", "System.Boolean", "System.Boolean", "System.Boolean", "System.Boolean"}, function(instance, ptable)
---     if ptable.ReturnValue == true and string.find(ptable["item"].tags, "petfood%w") then
---         local petIsNearby = false
---         for pet in SBAI.PetList do
---             petIsNearby = petIsNearby or (AIObjective.GetDistanceFactor(pet.WorldPosition, ptable["item"].WorldPosition, 0, 3, 200) ~= 0)
---         end
---         return not petIsNearby
---     end
--- end, Hook['HookMethodType'].After)
