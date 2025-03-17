5cd22e6a0ea50f22e2f685d7eb9c33debaeccd96
Dale Stager <dstager.lb@gmail.com>
2025-03-16 22:46:43 -0400

    Reorganized to help with future modules Optimized AiObjectiveDeconstructItems patch

--- Lua/Autorun/SBAI.lua
+++ Lua/Autorun/SBAI.lua
@@ -1,6 +1,9 @@
 if SERVER or Game.IsSingleplayer then
     SBAI = {}
     SBAI.Path = ...
+    SBAI.Namespace = {"SBAI"}
+    function SBAI.GetNamespace() return table.concat(SBAI.Namespace, ".") end
+
     dofile(SBAI.Path.."/Lua/SBAI/Server/ItemHelper.Lua")
     dofile(SBAI.Path.."/Lua/SBAI/Server/AiObjective.Lua")
 end
--- Lua/SBAI/Server/AIObjective.lua
+++ Lua/SBAI/Server/AIObjective.lua
@@ -1,39 +1,59 @@
+table.insert(SBAI.Namespace, "AIObjective")
+
+local function Reset()
+    -- bought a new sub, some mod adds deployable deconstructors, etc.
+    SBAI.playerSubmarineHasDeconstructor = nil --[[@type boolean|nil]]
+end
+
+Hook.Add("roundEnd", SBAI.GetNamespace()..".Reset", Reset)
+Reset()
+
 LuaUserData.RegisterType("Barotrauma.AIObjectiveDeconstructItem") 
 
 -- Deconstruct Items: Deconstruct only within the sub if deconstructor is within sub, with optimized calls to get all deconstructors in map
-Hook.Patch("SBAI.FindDeconstructor", "Barotrauma.AIObjectiveDeconstructItem", "FindDeconstructor", function(instance, ptable)
+Hook.Patch(SBAI.GetNamespace()..".UseShipDeconstructorIfAvailable", "Barotrauma.AIObjectiveDeconstructItem", "FindDeconstructor", function(instance, ptable)
     ptable.PreventExecution = true
 
-    local closestDeconstructor = nil
-    local bestDistFactor = 0
+    local closestDeconstructor = nil --[[@type Barotrauma.Items.Components.Deconstructor|nil]]
+    local bestDistFactor = 0.0 --[[@type System.Single]]
 
-    for attempt = 1, 2, 1 do
-        for i, potentialDeconstructor in ipairs(SBAI.DeconstructorList) do
-            local distFactor
-            if potentialDeconstructor == nil then goto continue end
-            if not potentialDeconstructor.InputContainer.Inventory.CanBePut(instance.Item) then goto continue end
-            if not potentialDeconstructor.Item.HasAccess(instance.character) then goto continue end
-            distFactor = AIObjective.GetDistanceFactor(instance.Item.WorldPosition, potentialDeconstructor.Item.WorldPosition, 0.2)
+    if SBAI.playerSubmarineHasDeconstructor == nil then
+        local i, deconstructorItem = next(SBAI.itemGroups["allDeconstructors"], nil)
+
+        while i and not SBAI.playerSubmarineHasDeconstructor do
+            SBAI.playerSubmarineHasDeconstructor = deconstructorItem.InPlayerSubmarine
+            i, deconstructorItem = next(SBAI.itemGroups["allDeconstructors"], i)
+        end
+    end
+
+    local deconstructor --[[@type Barotrauma.Items.Components.Deconstructor]]
+    local distFactor --[[@type System.Single]]
+
+    for _, deconstructorItem in ipairs(SBAI.GetUsableDeconstructors()) do
+        if deconstructorItem == nil then goto continue end
+        if SBAI.playerSubmarineHasDeconstructor and not deconstructorItem.InPlayerSubmarine then goto continue end
+        deconstructor = deconstructorItem.GetComponentString("Deconstructor") --[[@type Barotrauma.Items.Components.Deconstructor]]
+        if not deconstructor.InputContainer.Inventory.CanBePut(instance.Item) then goto continue end
+        if not deconstructorItem.HasAccess(instance.character) then goto continue end
+
+        distFactor = AIObjective.GetDistanceFactor(instance.Item.WorldPosition, deconstructorItem.WorldPosition, 0.2)
             if distFactor > bestDistFactor then
-                closestDeconstructor = potentialDeconstructor
+            closestDeconstructor = deconstructor
                 bestDistFactor = distFactor
             end
             ::continue::
         end
-        
-        -- regenerate the deconstructor list the old way if failed
-        if closestDeconstructor == nil and attempt == 1 then SBAI.GenerateDeconstructorList() end
-    end
-
     return closestDeconstructor
 end, Hook['HookMethodType'].Before)
 
 -- Fight Intruders: Prevent attacking any handcuffed people, regardless of being knocked down
-Hook.Patch("SBAI.PreventAttackingHandcuffedFightIntruders", "Barotrauma.AIObjectiveFightIntruders", "IsValidTarget", {"Barotrauma.Character"}, function(instance, ptable)
+Hook.Patch(SBAI.GetNamespace()..".PreventAttackingHandcuffed", "Barotrauma.AIObjectiveFightIntruders", "IsValidTarget", {"Barotrauma.Character"}, function(_, ptable)
     return ptable.ReturnValue and not ptable["target"].IsHandcuffed
 end, Hook['HookMethodType'].After)
 
 -- Operate Weapons: Prevent attacking handcuffed people
-Hook.Patch("SBAI.PreventAttackingHandcuffedCombat", "Barotrauma.AIObjectiveCombat", "GetPriority", function(instance, ptable)
+Hook.Patch(SBAI.GetNamespace()..".PreventAttackingHandcuffed", "Barotrauma.AIObjectiveCombat", "GetPriority", function(instance, _)
     if instance.Enemy.IsHandcuffed then return 0 end
 end, Hook['HookMethodType'].Before)
+
+table.remove(SBAI.Namespace)
