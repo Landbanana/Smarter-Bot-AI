76281e4ad510cc133d072e37cd2db123cda84484
Dale Stager <dstager.lb@gmail.com>
2025-04-12 12:40:43 -0400

    Removed old code

--- Lua/Autorun/Init.lua
+++ Lua/Autorun/Init.lua
@@ -9,12 +9,3 @@ if CLIENT or Game.IsSingleplayer then
 
     Activate(SBAI.namespace, SBAI.Config.data)
 end
-
-LuaUserData.MakeFieldAccessible(Descriptors["Barotrauma.AIObjectiveManager"], "character")
-
-Hook.Patch("test", "Barotrauma.AIObjectiveManager", "CreateAutonomousObjectives",
----@param instance Barotrauma.AIObjectiveManager
----@param ptable Barotrauma.LuaCsHook.ParameterTable
-function(instance, ptable)
-    print(instance.character.Name)
-end, Hook.HookMethodType.Before)
