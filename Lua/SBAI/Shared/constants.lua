43083604c32d031be2f946d3b39102d501a01a53
Dale Stager <dstager.lb@gmail.com>
2025-04-12 19:30:35 -0400

    Updated to 1.4.0

--- Lua/SBAI/Shared/constants.lua
+++ Lua/SBAI/Shared/constants.lua
@@ -1,7 +1,7 @@
 local Constants = {
     Name="Smarter Bot AI",
     Acronym="SBAI",
-    Version="1.3.2",
+    Version="1.4.0",
     Path=...,
     ModConfigsDirPath = Game.SaveFolder.."/ModConfigs", --[[@type string]]
     D_TIMER_NOISE = 0.1
