3571dd166a5aa3ef0d9f032d4812921f25becb13
Dale Stager <dstager.lb@gmail.com>
2025-04-16 10:59:06 -0400

    Updated to 1.4.2 (Barotrauma Hotfix 1.8.7.0)

--- Lua/SBAI/Shared/constants.lua
+++ Lua/SBAI/Shared/constants.lua
@@ -1,7 +1,7 @@
 local Constants = {
     Name="Smarter Bot AI",
     Acronym="SBAI",
-    Version="1.4.1",
+    Version="1.4.2",
     Path=table.pack(...)[1] --[[@type string]],
     ModConfigsDirPath=Game.SaveFolder.."/ModConfigs", --[[@type string]]
     D_TIMER_NOISE=0.1
