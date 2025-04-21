effb83950e1a209564e8670bb4ebcf4d462ca171
Dale Stager <dstager.lb@gmail.com>
2025-04-21 19:49:23 -0400

    Turned some globals into locals

--- Lua/SBAI/Server/Modules/UseTalents.lua
+++ Lua/SBAI/Server/Modules/UseTalents.lua
@@ -57,90 +57,107 @@ local instrumentTalentData = {
 }
 local instrumentTalentEnabled = false
 local anyStopAfterBuffed = false
+local playInstruments
 
----@param self Types.Module
----@param character Barotrauma.Character
----@param instrumentIds string[]
-local function playInstruments(self, character, instrumentIds)
-    local characterData = allCharacterData[character]
-    local instance = character.AIController.objectiveManager.CurrentObjective --[[@type Barotrauma.AIObjective]]
+do
+    local Aim = InputType.Aim
+    local Shoot = InputType.Shoot
 
-    if instance ~= characterData.lastObjective then
-        characterData.lastObjective = instance
-        instance.Deselected.add(
-        function()
-            return characterData:Reset()
-        end)
-    end
+    local FindItem = util.FindItem
+    local GenerateIdPredicate = util.GenerateIdPredicate
+    local HasSimpleAccess = util.HasSimpleAccess
+    local ValsContain = util.ValsContain
 
-    local instrument = characterData.instrument
-    local inventory = character.Inventory
-    
-    if  not (
-            instrument and
-            util.HasSimpleAccess(character, instrument)
-        )
-    then
-        characterData.instrument = util.FindItem(character, inventory.FindAllItems(), nil, nil, util.GenerateIdPredicate(instrumentIds))
-    end
+    ---@param self Types.Module
+    ---@param character Barotrauma.Character
+    ---@param instrumentIds string[]
+    function playInstruments(self, character, instrumentIds)
+        local characterData = allCharacterData[character]
+        local instance = character.AIController.objectiveManager.CurrentObjective --[[@type Barotrauma.AIObjective]]
 
-    if  instrument and (
-            util.ValsContain(character.HeldItems, instrument) or
-            inventory.TryPutItem(instrument, character, instrumentData[instrument.Prefab.Identifier.Value], true, false)
-        )
-    then
-        character.SetInput(InputType.Aim, false, true)
-        character.SetInput(InputType.Shoot, false, true)
-    else
-        characterData.isPlaying = false
+        if instance ~= characterData.lastObjective then
+            characterData.lastObjective = instance
+            instance.Deselected.add(
+            function()
+                return characterData:Reset()
+            end)
+        end
+
+        local instrument = characterData.instrument
+        local inventory = character.Inventory
+        
+        if  not (
+                instrument and
+                HasSimpleAccess(character, instrument)
+            )
+        then
+            characterData.instrument = FindItem(character, inventory.FindAllItems(), nil, nil, GenerateIdPredicate(instrumentIds))
+        end
+
+        if  instrument and (
+                ValsContain(character.HeldItems, instrument) or
+                inventory.TryPutItem(instrument, character, instrumentData[instrument.Prefab.Identifier.Value], true, false)
+            )
+        then
+            character.SetInput(Aim, false, true)
+            character.SetInput(Shoot, false, true)
+        else
+            characterData.isPlaying = false
+        end
     end
 end
 
-local function generateWanderPatch(self, talentId, stopAfterBuffed)
-    if stopAfterBuffed then
-        anyStopAfterBuffed = true
+local generateWanderPatch
 
-        ---@param instance Barotrauma.AIObjectiveIdle
-        ---@param ptable Barotrauma.LuaCsHook.ParameterTable
-        return function(instance, ptable)
-            local character = instance.character
-            local characterData = allCharacterData[character]
+do
+    local Distance = Vector2.Distance
 
-            if character.HasTalent(talentId) then
-                if characterData.timer:Update(ptable["deltaTime"]) then
-                    local startPos = character.WorldPosition
-                    local range = talentRanges[talentId]
-                    local foundUnbuffed = false
+    function generateWanderPatch(self, talentId, stopAfterBuffed)
+        if stopAfterBuffed then
+            anyStopAfterBuffed = true
 
-                    for crewmate in Character.GetFriendlyCrew(character) do
-                        if not crewmate.CharacterHealth.GetAffliction(instrumentTalentData[talentId].afflictionId, false) and
-                            Vector2.Distance(startPos, crewmate.WorldPosition) <= range
-                        then
-                            foundUnbuffed = true
-                            break
+            ---@param instance Barotrauma.AIObjectiveIdle
+            ---@param ptable Barotrauma.LuaCsHook.ParameterTable
+            return function(instance, ptable)
+                local character = instance.character
+                local characterData = allCharacterData[character]
+
+                if character.HasTalent(talentId) then
+                    if characterData.timer:Update(ptable["deltaTime"]) then
+                        local startPos = character.WorldPosition
+                        local range = talentRanges[talentId]
+                        local foundUnbuffed = false
+
+                        for crewmate in Character.GetFriendlyCrew(character) do
+                            if not crewmate.CharacterHealth.GetAffliction(instrumentTalentData[talentId].afflictionId, false) and
+                                Distance(startPos, crewmate.WorldPosition) <= range
+                            then
+                                foundUnbuffed = true
+                                break
+                            end
+                        end
+                        if foundUnbuffed then
+                            characterData.isPlaying = true
+                        else
+                            characterData:Reset()
                         end
                     end
-                    if foundUnbuffed then
-                        characterData.isPlaying = true
-                    else
-                        characterData:Reset()
+                    if characterData.isPlaying then
+                        playInstruments(self, character, instrumentTalentData[talentId].validInstruments)
                     end
                 end
-                if characterData.isPlaying then
+            end
+        else
+            ---@param instance Barotrauma.AIObjectiveIdle
+            ---@param ptable Barotrauma.LuaCsHook.ParameterTable
+            return function(instance, ptable)
+                local character = instance.character
+                local characterData = allCharacterData[character]
+        
+                if character.HasTalent(talentId) then
+                    characterData.isPlaying = true
                     playInstruments(self, character, instrumentTalentData[talentId].validInstruments)
                 end
-            end
-        end
-    else
-        ---@param instance Barotrauma.AIObjectiveIdle
-        ---@param ptable Barotrauma.LuaCsHook.ParameterTable
-        return function(instance, ptable)
-            local character = instance.character
-            local characterData = allCharacterData[character]
-    
-            if character.HasTalent(talentId) then
-                characterData.isPlaying = true
-                playInstruments(self, character, instrumentTalentData[talentId].validInstruments)
             end
         end
     end
@@ -234,6 +251,8 @@ local function activate(self)
         if anyStopAfterBuffed then
             local baseCharacterDataIndex = characterDataIndex
             local buffCheckDelay = self.options["timeBetween"] --[[@type number]]
+
+            local Timer = Types.Timer
             
             ---@param t table<Barotrauma.Character,{instrument:Barotrauma.Item?, isPlaying:boolean, lastObjective:Barotrauma.AIObjective, Reset:fun(self), ["timer"]:Types.Timer}>
             ---@param k Barotrauma.Character
@@ -241,7 +260,7 @@ local function activate(self)
             characterDataIndex = function(t, k)
                 t[k] = baseCharacterDataIndex(t, k)
 
-                t[k].timer = Types.Timer.new(buffCheckDelay)
+                t[k].timer = Timer.new(buffCheckDelay)
                 return t[k]
             end
 
