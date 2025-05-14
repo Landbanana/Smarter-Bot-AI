fa3e2cc17f027bbb65776a17a6887d7900aa7cfd
Dale Stager <dstager.lb@gmail.com>
2025-05-14 11:18:50 -0400

    Updateds to 1.6.3d

--- Lua/SBAI/Shared/constants.lua
+++ Lua/SBAI/Shared/constants.lua
@@ -1,7 +1,7 @@
 local Constants = {
     Name="Smarter Bot AI",
     Acronym="SBAI",
-    Version="1.6.3c",
+    Version="1.6.3d",
     Path=ToolBox.CleanUpPath(table.pack(...)[1]),
     ModConfigsDirPath=ToolBox.CleanUpPath(Game.SaveFolder.."/ModConfigs"), --[[@type string]]
 
