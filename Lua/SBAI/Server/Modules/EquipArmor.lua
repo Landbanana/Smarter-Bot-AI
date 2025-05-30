b0fa7ef4886bc5ac38f3f88550ed37daf3b65cd7
Dale Stager <dstager.lb@gmail.com>
2025-05-30 17:08:33 -0400

    Added headset to "Equip Items" module

--- Lua/SBAI/Server/Modules/EquipArmor.lua
+++ Lua/SBAI/Server/Modules/EquipArmor.lua
@@ -10,19 +10,19 @@ local function activate(self)
     local Wearable = Components.Wearable
     
     local Any = util.itertools.Any
-    local band = bit32.band
     local FilterList = util.itertools.FilterList
+    local HasFlag = util.mathtools.HasFlag
     local Contains = util.itertools.Contains
 
-    local clothesSlotTypes = {InvSlotType.Head, InvSlotType.InnerClothes, InvSlotType.OuterClothes}
-    local allCharacterData = Types.TimedCharacterData.new(self)
+    local clothesSlotTypes = {InvSlotType.Head, InvSlotType.InnerClothes, InvSlotType.OuterClothes, InvSlotType.Headset}
+    local allCharacterData = Types.AllTimedCharacterData.new(self)
 
     self:AddPatch("Barotrauma.AIObjectiveIdle", "Act", nil,
     function(instance, ptable)
         local character = instance.character --[[@type Barotrauma.Character]]
         
         if  character.IsHuman and
-            allCharacterData:Get(character).timer:Update(ptable["deltaTime"])
+            allCharacterData:Get(character):Update(ptable["deltaTime"])
         then
             local inventory = character.Inventory --[[@type Barotrauma.CharacterInventory]]
             local filteredClothesSlotTypes = 0
@@ -36,16 +36,16 @@ local function activate(self)
             ---@param slot Barotrauma.InvSlotType
             ---@return boolean
             local function testSlot(slot)
-                return band(filteredClothesSlotTypes, slot) == slot
+                return HasFlag(filteredClothesSlotTypes, slot)
             end
 
             for item in inventory:SBAI_findAllItems(nil, true,
                 function(item)
                     return item.GetComponent(Wearable) ~= nil and
-                    not item.HasTag(lightdivingId) and
-                    not item.HasTag(deepdivingId) and
-                    not Contains(character.HeldItems, item) and
-                    Any(item.AllowedSlots, testSlot)
+                        not item.HasTag(lightdivingId) and
+                        not item.HasTag(deepdivingId) and
+                        not Contains(character.HeldItems, item) and
+                        Any(item.AllowedSlots, testSlot)
                 end) do
                 for v in FilterList(item.AllowedSlots, testSlot) do
                     if inventory.TryPutItem(item, character, {v}, true, true) then
