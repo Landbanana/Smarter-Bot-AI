b3246bc1d81ab8fc7ce2fab111bae21c3865ce77
Dale Stager <dstager.lb@gmail.com>
2025-06-13 23:13:44 -0400

    Updated to 1.7.1d

--- Lua/SBAI/Shared/constants.lua
+++ Lua/SBAI/Shared/constants.lua
@@ -3,7 +3,7 @@ LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.CharacterInventory"], "
 local Constants = {
     Name="Smarter Bot AI",
     Acronym="SBAI",
-    Version="1.7.1c",
+    Version="1.7.1d",
     Path=ToolBox.CleanUpPath(table.pack(...)[1]),
     ModConfigsDirPath=ToolBox.CleanUpPath(Game.SaveFolder.."/ModConfigs"), --[[@type string]]
 
